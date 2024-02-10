1c6e8f19927b8bad508475189616177cd29b2966
amina-work <mina.works28@gmail.com>
2024-02-10 16:43:10 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # visualizing data
+import seaborn as sns # visualizing data with stunning default theme
+import sklearn # contain algorithms
+import warnings
+warnings.filterwarnings('ignore')
