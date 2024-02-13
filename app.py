614e9f3b82503255f0ce3bbc06c49f687e8f1272
amina-work <mina.works28@gmail.com>
2024-02-13 12:15:53 +0100

    added zip extraction python library

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ import matplotlib.pyplot as plt # visualizing data
 import seaborn as sns # visualizing data with stunning default theme
 import sklearn # contain algorithms
 import warnings
+import zipfile #to treat data that's in a zip file.
+import os
 warnings.filterwarnings('ignore')
+
+# Path to your zip file
+zip_file_path = 'path/to/your/archive.zip'
+
+# Directory where you want to extract the files
+extract_dir = 'path/to/extract'
+# Open the zip file
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    # Extract all the contents into the specified directory
+    zip_ref.extractall(extract_dir)
+print("Extraction complete.")
